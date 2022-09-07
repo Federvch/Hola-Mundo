9f054e52f471f5afe26fdff15305f1cfc9f591b4
Feder <vch.federico@gmail.com>
2022-09-06 22:02:51 -0300

    Guardado 06/09/2022

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author A274929
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+// Ejercio 1 video
+/*
+Scanner leer = new Scanner (System.in);
+
+String nombre;
+
+System.out.println( "Ingresa tu nombre");
+        
+nombre = leer.next();
+
+System.out.print( "Hola Mundo! Soy "+ nombre + " y estoy programando en java");
+
+}
+}
+*/
+
+// Ejercicio 1 Guia 6 Deteccion de Errores
+/*
+    String nombre;
+    boolean bandera;
+    char Letra;
+}
+}
+*/
+
+//Ejercicio 2 Guia 6 Deteccion de Errores
+ /*  int numero = 48;
+    double decimales = 3.55;
+    boolean bandera = false;
+}
+
+}
+*/
+ 
+//Ejercicio 3 Guia 6 Define variables donde puedas alojar los resultados y prueba usar dos operadores de cada tip
+/*
+    int num1 = 5;
+    int num2 = 10;
+    
+    int suma;
+     
+    suma = num1 + num2;
+            
+    double division = num1 / num2;
+        
+    boolean bandera = num2 < num1;*/
+
+// Ejercicio 4 Define una variable que aloje tu nombre y otra que guarde tu edad. Imprime ambas variables por pantalla.
+
+
+String nombre = " Feder";
+
+int edad;
+
+Scanner leer = new Scanner(System.in);
+
+System.out.println(" Ingrese su edad");
+
+edad = leer.nextInt();
+
+System.out.println( " Hola " + nombre + " tenes " + edad + " años");
+        
+        
+}
+    }
+
+        
+
+
+   
+    
+
+    
+
+            
